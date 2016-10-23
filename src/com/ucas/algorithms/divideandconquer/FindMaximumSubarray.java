d6f7b127ded9a1288513f18fa87254dc4f9cd0cf
jinge <wjg172184@163.com>
2016-10-23 22:39:18 +0800

    添加最大子数组问题。未完成。 添加多返回值工具类，以满足多个返回值的需求。未完成。

---
+++ src/com/ucas/algorithms/divideandconquer/FindMaximumSubarray.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.ucas.algorithms.divideandconquer;
+
+/**
+ * @author jinge
+ *
+ */
+public class FindMaximumSubarray {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public findMaximumSubarray(int[] arr, int low, int high) {
+		if (high == low) {
+			return 
+		}
+	}
+	
+}
