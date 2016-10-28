efed31a5784795801d2fc0726a359cbf26137f0f
jinge <wjg172184@163.com>
2016-10-28 18:21:01 +0800

    测试不同输入规模下两个矩阵乘法器的效率。

--- src/com/ucas/algorithms/matrix/MatrixMultiplyTest.java
+++ src/com/ucas/algorithms/matrix/MatrixMultiplyTest.java
@@ -10,22 +10,28 @@ public class MatrixMultiplyTest {
 
 	public static void main(String[] args) {
 		
-		Matrix A = MatrixGenerator.generateRandomMatrix(1024);
-		Matrix B = MatrixGenerator.generateRandomMatrix(1024);
-		TimeElapse time1 = new TimeElapse();
-		time1.start();
-		Matrix plainC = A.multiply(B, MultiplierType.PLAIN);
-		time1.stop();
-		time1.showElapsedTime();
-		TimeElapse time2 = new TimeElapse();
-		time2.start();
-		Matrix strassenC = A.multiply(B, MultiplierType.STRASSEN);
-		time2.stop();
-		time2.showElapsedTime();
+		for (int n=0; n<12; n++) {
+		
+			Matrix A = MatrixGenerator.generateRandomMatrix(1 << n);
+			Matrix B = MatrixGenerator.generateRandomMatrix(1 << n);
+			TimeElapse time1 = new TimeElapse();
+			time1.start();
+			Matrix plainC = A.multiply(B, MultiplierType.PLAIN);
+			time1.stop();
+			System.out.print(n + "plain: ");
+			time1.showElapsedTime();
+			TimeElapse time2 = new TimeElapse();
+			time2.start();
+			Matrix strassenC = A.multiply(B, MultiplierType.STRASSEN);
+			time2.stop();
+			System.out.print(n + "strassen: ");
+			time2.showElapsedTime();
 //		MatrixPrinter.print(A);
 //		MatrixPrinter.print(B);
 //		MatrixPrinter.print(plainC);
 //		MatrixPrinter.print(strassenC);
+		
+		}
 		
 	}
 
