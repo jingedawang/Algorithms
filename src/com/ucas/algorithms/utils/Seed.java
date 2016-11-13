67727708c88b4f9a906029bfb47caaac3fa73abe
jinge <wjg172184@163.com>
2016-11-13 20:43:55 +0800

    完成randomInPlace算法。 添加文档注释。

--- src/com/ucas/algorithms/utils/Seed.java
+++ src/com/ucas/algorithms/utils/Seed.java
@@ -2,10 +2,19 @@ package com.ucas.algorithms.utils;
 
 import java.util.Random;
 
+/**
+ * 种子生成器，在同一应用程序中生成不重复的种子，可用于随机数生成器。
+ * @author wjg
+ *
+ */
 public class Seed {
 
 	private static Random random = new Random(System.currentTimeMillis());
 	
+	/**
+	 * 获得一个种子
+	 * @return 种子
+	 */
 	public static long next() {
 		return random.nextLong();
 	}
--- src/randomize/RandomizeArray.java
+++ src/randomize/RandomizeArray.java
@@ -2,19 +2,47 @@ package randomize;
 
 import java.util.Random;
 
+import com.ucas.algorithms.sort.SortByPriority;
+import com.ucas.algorithms.utils.ArrayPrinter;
 import com.ucas.algorithms.utils.IntegerArrayGenerator;
+import com.ucas.algorithms.utils.Seed;
 
 public class RandomizeArray {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int[] arr = IntegerArrayGenerator.fixedArray();
+		ArrayPrinter.print(arr);
+		randomizeInPlace(arr);
+		ArrayPrinter.print(arr);
+		
+		
 	}
 	
+	/**
+	 * 使用随机产生的优先级数组生成随机排列（未完成）
+	 * @param arr 需要随机排列的数组
+	 */
 	public static void permuteBySorting(int[] arr) {
 		int n = arr.length;
 		int[] P = IntegerArrayGenerator.randomArray(n * n * n, n);
-		
+		new SortByPriority().sortByPriority(arr, P);
+	}
+	
+	/**
+	 * 使用随机交换元素的方式生成随机排列
+	 * @param arr 需要随机排列的数组
+	 */
+	public static void randomizeInPlace(int[] arr) {
+		int n = arr.length;
+		Random random = new Random(Seed.next());
+		for (int i=0; i<n; i++) {
+			int randomIndex = random.nextInt(n - i) + i;
+			int temp = arr[i];
+			arr[i] = arr[randomIndex];
+			arr[randomIndex] = temp;
+		}
 	}
 
 }
