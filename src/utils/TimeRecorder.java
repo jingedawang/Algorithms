a43bdc8b6531c1c4ea5b262f2b9bdb707fb42eca
Jinge Wang <wjg172184@163.com>
2022-04-25 21:42:18 +0800

    Implement multi-stage mode in TimeRecorder.

--- src/utils/TimeRecorder.java
+++ src/utils/TimeRecorder.java
@@ -7,6 +7,39 @@ import java.util.ArrayList;
 
 /**
  * Record time consumption of code pieces and print the elapsed time in a pretty format.
+ *
+ * Two usages are provided, please refer to following examples.
+ * <p/>
+ * Usage 1: Simple record mode
+ * <pre>
+ * TimeRecorder timeRecorder = new TimeRecorder();
+ * timeRecorder.start();
+ * // Do your job here...
+ * timeRecorder.stop();
+ * timeRecorder.print();
+ * </pre>
+ * The output in the console should like<p>
+ * {@code Time used: 13ms used.}
+ * <p/>
+ * Usage 2: Multi-stage record mode
+ * <pre>
+ * TimeRecorder timeRecorder = new TimeRecorder("My workflow");
+ * timeRecorder.start();
+ * // Do the initialization here...
+ * timeRecorder.addTimePoint("Initialize");
+ * // Execute the job here...
+ * timeRecorder.addTimePoint("Execute");
+ * // Release the resources here...
+ * timeRecorder.addTimePoint("Release");
+ * timeRecorder.print();
+ * </pre>
+ * The output in the console should like<p>
+ * <pre>
+ * -----------------------My workflow---------------------
+ * --    Initialize      Execute      Release        Total
+ * --          15ms         31ms          6ms         52ms
+ * -------------------------------------------------------
+ * </pre>
  */
 public class TimeRecorder {
 
@@ -48,7 +81,10 @@ public class TimeRecorder {
 	 * Stop timing.
 	 */
 	public void stop() {
-		addTimePoint("end_time");
+		// Add end time point only if user didn't add any extra time points.
+		if (names.size() == 1) {
+			addTimePoint("end_time");
+		}
 	}
 
 	/**
@@ -63,26 +99,59 @@ public class TimeRecorder {
 	 * Show elapsed time.
 	 */
 	public void print() {
+		System.out.println(summary(title, names));
+	}
+
+	/**
+	 * Get the summary text of the time consumption.
+	 * @return A summary string.
+	 */
+	public String summary() {
+		return summary(title, names);
+	}
+
+	private String summary(String title, ArrayList<String> names) {
+		// Compute time intervals for the time points.
 		ArrayList<Integer> timeIntervals = new ArrayList<>();
 		for (int i = 1; i < names.size(); i++) {
 			timeIntervals.add(Math.toIntExact((timePoints.get(i) - timePoints.get(i - 1)) / 1000000));
 		}
-		print(title, names, timeIntervals);
-	}
 
-	private void print(String title, ArrayList<String> names, ArrayList<Integer> timeIntervals) {
+		// Generate the summary string.
 		assert names.size() == timeIntervals.size() + 1;
 		if (timeIntervals.size() == 1) {
-			System.out.println(title + ": " + timeIntervals.get(0) + "ms used.");
+			return title + ": " + timeIntervals.get(0) + "ms used.";
 		}
 		else {
-			// TODO: Pretty print used time.
+			StringBuilder stringBuilder = new StringBuilder();
+			stringBuilder.append("---");
+			for (int i = 0; i < (names.size() * 13 - title.length()) / 2; i++) {
+				stringBuilder.append('-');
+			}
+			stringBuilder.append(title);
+			for (int i = 0; i < (names.size() * 13 - title.length() + 1) / 2; i++) {
+				stringBuilder.append('-');
+			}
+			stringBuilder.append("\n-- ");
+			for (int i = 1; i < names.size(); i++) {
+				stringBuilder.append(String.format("%13s", names.get(i)));
+			}
+			stringBuilder.append(String.format("%13s", "Total"));
+			stringBuilder.append("\n-- ");
+			for (int timeInterval : timeIntervals) {
+				stringBuilder.append(String.format("%11dms", timeInterval));
+			}
+			stringBuilder.append(String.format("%11dms", timeIntervals.stream().reduce(0, Integer::sum)));
+			stringBuilder.append("\n---");
+			for (int i = 0; i < names.size() * 13; i++) {
+				stringBuilder.append('-');
+			}
+
+			return stringBuilder.toString();
 		}
 	}
 
 	private final String title;
-	private long startTime;
-	private long endTime;
 	private final ArrayList<String> names = new ArrayList<>();
 	private final ArrayList<Long> timePoints = new ArrayList<>();
 
---
+++ test/TimeRecorderTest.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.Test;
+import utils.TimeRecorder;
+
+/**
+ * Test class for {@link TimeRecorder}.
+ */
+public class TimeRecorderTest {
+
+	@Test
+	void print() throws InterruptedException {
+		TimeRecorder timeRecorderSimple = new TimeRecorder();
+		timeRecorderSimple.start();
+		Thread.sleep(10);
+		timeRecorderSimple.stop();
+		timeRecorderSimple.print();
+		timeRecorderSimple.summary();
+
+		TimeRecorder timeRecorderComplex = new TimeRecorder("My workflow");
+		timeRecorderComplex.start();
+		Thread.sleep(10);
+		timeRecorderComplex.addTimePoint("Initialize");
+		Thread.sleep(30);
+		timeRecorderComplex.addTimePoint("Execute");
+		Thread.sleep(5);
+		timeRecorderComplex.addTimePoint("Release");
+		timeRecorderComplex.stop();
+		timeRecorderComplex.print();
+		timeRecorderComplex.summary();
+	}
+
+}
